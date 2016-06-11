31e1554e60d4a5028dd1da05c36a3569a6f2a566
gregoiredelzongle <gregoiredelzongle@macbook-pro-de-gregoire.home>
2016-06-11 11:44:18 +0200

    Added "SplineObjectPlacer"
    Simple component to lock game object on the spline

---
+++ Scripts/Editor/SplineObjectPlacerInspector.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEditor;
+using GPGre.QuadraticBezier;
+
+[CustomEditor(typeof(SplineObjectPlacer))]
+public class SplineObjectPlacerInspector : Editor {
+
+	SplineObjectPlacer script;
+
+	void OnEnable()
+	{
+		script = target as SplineObjectPlacer;
+	}
+
+	override public void OnInspectorGUI()
+	{
+		script.spline = (BezierSpline)EditorGUILayout.ObjectField (script.spline, typeof(BezierSpline), true);
+
+		if (script.spline == null)
+			return;
+
+		EditorGUI.BeginChangeCheck ();
+		float positionValue = EditorGUILayout.Slider(script.PositionOnSpline,0,1);
+		if (EditorGUI.EndChangeCheck ()) {
+			script.PositionOnSpline = positionValue;
+		}
+
+		EditorGUI.BeginChangeCheck ();
+		script.followCurveDirection = EditorGUILayout.ToggleLeft("Follow Curve Direction",script.followCurveDirection);
+		if (EditorGUI.EndChangeCheck ()) {
+			script.PositionOnSpline = positionValue;
+		}
+
+
+	}
+}
---
+++ Scripts/SplineObjectPlacer.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using GPGre.QuadraticBezier;
+
+namespace GPGre.QuadraticBezier
+{
+	/// <summary>
+	/// Place an object on spline
+	/// </summary>
+	public class SplineObjectPlacer : MonoBehaviour {
+
+		public BezierSpline spline;
+
+		public float PositionOnSpline 
+		{
+			get{ return _positionOnSpline; 
+			}
+			set{
+				PositionObjectOnSpline (value);
+				_positionOnSpline = value;
+			}
+		}
+		[SerializeField,HideInInspector]
+		private float _positionOnSpline = 0;
+
+		public bool followCurveDirection = true;
+
+		void PositionObjectOnSpline(float newPosition)
+		{
+			transform.position = spline.GetPointUniform (newPosition);
+			if(followCurveDirection)
+				transform.rotation = Quaternion.LookRotation(spline.GetDirectionUniform(newPosition));
+		}
+	}
+}
